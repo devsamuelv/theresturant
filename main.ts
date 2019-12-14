1e162ff13a1ca021130979a66e41de57817d9db1
DevSamuelV <realalloutmusic@gmail.com>
2019-12-14 00:15:50 -0500

    nothing

---
+++ main.ts
@@ -0,0 +1,20 @@
+// let slogan = document.getElementById('slogan-title');
+// let tab_title = document.getElementById('tab-title');
+
+// let is_english:boolean = true;
+// let is_spanish:boolean = false;
+
+// function lang() {
+//     if (is_english == false) {
+//         slogan.innerHTML = "Good Food. Good Friends. Good Times.";
+//         is_english = true;
+//         is_spanish = false;
+//     }
+
+//     if (is_spanish == true) {
+//         slogan.innerHTML = "Buena comida. Buenos amigos. Buenos tiempos.";
+//         tab_title.innerHTML = " | casa";
+//         is_english = false;
+//         is_spanish = true;
+//     }
+// }
